7e4d260aa018dfc74a043f29b295b512384e6da5
iro <iro@sitecore.net>
2019-04-02 13:12:55 +0300

    Initial

---
+++ src/Sitecore.Support.321854/SitemapGenerator.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.Support.XA.Feature.SiteMetadata.Sitemap
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Globalization;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Xml;
+
+  public class SitemapGenerator : Sitecore.XA.Feature.SiteMetadata.Sitemap.SitemapGenerator
+  {
+    public SitemapGenerator() : base() { }
+
+    public SitemapGenerator(XmlWriterSettings xmlWriterSettings) : base(xmlWriterSettings)
+    {
+    }
+    protected override IEnumerable<Item> GetItemsForOtherLanguages(Item item)
+    {
+      foreach (Language item3 in from language in item.Languages
+        where language != item.Language
+        select language)
+      {
+        Item item2 = item.Database.GetItem(item.ID, item3);
+        if (item2.Versions.Count > 0)
+        {
+          yield return item2;
+        }
+      }
+    }
+    
+  }
+}
